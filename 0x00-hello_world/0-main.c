b002bbf4420371bf174302af72d9517266b02c4e
Victor.Macharia <machariamuriukivic@gmail.com>
2023-02-16 01:54:39 -0800

    nkt

--- 0x00-hello_world/0-main.c
+++ 0x00-hello_world/0-main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main () {
-	printf("HELLO, WORLD!");
 
-	return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Holberton School");
+	return (0);
 }
