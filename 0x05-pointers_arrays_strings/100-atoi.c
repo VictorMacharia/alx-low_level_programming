9883f6f7878e1b8e0f87d37b53fbb41762cc5dd0
VictorMacharia <machariamuriukivic@gmail.com>
2023-02-27 01:21:51 -0800

    Ufffff

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer.
+ * @s - points to the string to be converted
+ *
+ */
+
+int _atoi(char *s)
+{
+	int num, numcount, mult, a, length, neg;
+
+	length = 0;
+	a = 0;
+	numcount = 0;
+	mult = 1;
+	neg = 1;
+	num = 0;
+
+	while (s[length] != '\0')
+	{
+		if (s[length] >= '0' && s[length] <= '9')
+		{
+			numcount++;
+			if (!(s[length + 1] >= '0' && s[length + 1] <= '9'))
+				break;
+		}
+		length++;
+	}
+	for (; numcount > 1; numcount--)
+		mult *= 10;
+	for (; a <= length; a++)
+	{
+		if (s[a] == '-')
+			neg *= -1;
+		else if (s[a] <= '9' && s[a] >= '0')
+		{
+			num += (s[a] - '0') * mult * neg;
+			mult /= 10;
+		}
+	}
+	return(num);
+}
